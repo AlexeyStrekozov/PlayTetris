3e9b1cc2b644bbd763ffcb4bbd7030ae21020345
AlexeyStrekozov <strekozovalexey@gmail.com>
2019-03-29 01:16:13 +0600

    Фигуры плавно опускаются и реагируют на стрелки

--- script/script.js
+++ script/script.js
@@ -21,7 +21,7 @@ for (let y=18; y>0; y--) {
     }
 }
 
-let x = 5, y = 10;
+let x = 5, y = 15;
 let mainArr = [
     //плака
     [
@@ -36,6 +36,42 @@ let mainArr = [
         [0, 1],
         [1, 1]
     ],
+    
+    //буква L
+    [
+        [1, 0],
+        [0, 1],
+        [0, 2]
+    ],
+
+     //буква L зеркальная
+     [
+        [1, 0],
+        [1, 1],
+        [1, 2]
+    ],
+
+      //молния вправо
+      [
+        [1, 0],
+        [-1, 1],
+        [0, 1]
+    ],
+
+       //молния лево
+       [
+        [1, 0],
+        [1, 1],
+        [2, 1]
+    ],
+    
+         //лего
+         [
+            [1, 0],
+            [2, 0],
+            [1, 1]
+        ],
+    
 ]
 
 let currentFigure = 0;
@@ -56,8 +92,98 @@ function create() {
     ]
 
     for (let i = 0; i<figureBody.length; i++) {
-        figureBody[i].classList.add('figure')
+        figureBody[i].classList.add('figure');
+    }
+}
+create();
+
+function move(){
+    let moveFlag = true;
+    let coordinates = [
+        [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')],
+        [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')],
+        [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')],
+        [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')],
+    ];
+
+    for (let i=0; i<coordinates.length; i++) {
+        if (coordinates[i][1] == 1 || document.querySelector(`[posX = "${coordinates[i][0]}"][posY = "${coordinates[i][1] - 1}"]`).classList.contains('set')) {
+            moveFlag = false;
+            break;
+        }
+}
+        if (moveFlag) {
+            for (let i = 0; i < figureBody.length; i++) {
+                figureBody[i].classList.remove('figure');
+            }
+            figureBody = [
+                document.querySelector(`[posX = "${coordinates[0][0]}"][posY = "${coordinates[0][1] - 1}"]`),
+                document.querySelector(`[posX = "${coordinates[1][0]}"][posY = "${coordinates[1][1] - 1}"]`),
+                document.querySelector(`[posX = "${coordinates[2][0]}"][posY = "${coordinates[2][1] - 1}"]`),
+                document.querySelector(`[posX = "${coordinates[3][0]}"][posY = "${coordinates[3][1] - 1}"]`),
+            ];
+            for (let i = 0; i<figureBody.length; i++) {
+                figureBody[i].classList.add('figure');
+            }
+        } else {
+            for (let i = 0; i<figureBody.length; i++) {
+                figureBody[i].classList.remove('figure');
+                figureBody[i].classList.add('set');
+            }
+            create();
     }
 }
 
-create();
+let interval = setInterval(() => {
+    move();
+}, 300);
+
+let flag = true;
+
+window.addEventListener('keydown', function (e) {
+
+    let coordinates1 = [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')];
+    let coordinates2 = [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')];
+    let coordinates3 = [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')];
+    let coordinates4 = [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')];
+
+    function getNewState(a) {
+
+        flag = true;
+
+        let figureNew = [
+            document.querySelector(`[posX = "${+coordinates1[0] + a}"][posY = "${coordinates1[1]}"]`),
+            document.querySelector(`[posX = "${+coordinates2[0] + a}"][posY = "${coordinates2[1]}"]`),
+            document.querySelector(`[posX = "${+coordinates3[0] + a}"][posY = "${coordinates3[1]}"]`),
+            document.querySelector(`[posX = "${+coordinates4[0] + a}"][posY = "${coordinates4[1]}"]`),
+        ];
+
+        for (let i=0; i<figureNew.length; i++) {
+            if (!figureNew[i] || figureNew[i].classList.contains('set')){
+                flag = false;
+            }
+        }
+        
+        if (flag == true) {
+            for (let i = 0; i < figureBody.length; i++) {
+                figureBody[i].classList.remove('figure');
+            }
+
+            figureBody = figureNew;
+
+            for (let i = 0; i < figureBody.length; i++) {
+                figureBody[i].classList.add('figure');
+            }
+        }
+
+    }
+
+    if (e.keyCode == 37) {
+        getNewState(-1);
+    } else if (e.keyCode == 39) {
+        getNewState(1);
+    } else if (e.keyCode == 40) {
+        move();
+    }
+
+}) 
